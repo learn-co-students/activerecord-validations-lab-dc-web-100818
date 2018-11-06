ecedbccc72e4a33a5dc4c5235971f47677b22bd2
github username <github email address>
2018-11-05 21:18:09 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid option"}
+  validate :clickbaity
+
+  @@clickbait = [/Won't Believe/i, /Secret/i, /Top/i, /Guess/i]
+
+  def clickbaity
+    if @@clickbait.none? {|c| c.match title}
+      errors.add(:title, "Title need more clickbait")
+    end
+  end
+
+
+#regular validations
 end
